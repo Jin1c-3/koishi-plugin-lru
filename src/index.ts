aa48bf592d5cd2896566554171d6d42396f44520
Jin1c on imini <Jin0x31c@gmail.com>
2024-11-17 21:02:41 +0800

    增加本地化支持

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export interface Config {
 export const Config: Schema<Config> = Schema.object({
   relex_time: Schema.number()
     .role("slider")
-    .min(0)
+    .min(1.5)
     .max(10)
     .step(0.5)
     .default(2.5)
@@ -18,51 +18,51 @@ export const Config: Schema<Config> = Schema.object({
 });
 
 export function apply(ctx: Context, { relex_time }: Config) {
+  ctx.i18n.define("zh-CN", require("./locales/zh-CN"));
   ctx = ctx.platform("onebot").guild();
-
   ctx
-    .command(
-      "lru <threshold:number>",
-      "踢出太久没有发言的群友，必要函参要踢的人数",
-      {
-        authority: 3,
-      }
-    )
+    .command("lru <threshold:number>", {
+      authority: 3,
+    })
     .option("dry", "只检测不踢人")
     .option("no-title", "不踢有头衔的群友")
     .action(async ({ session, options }, threshold) => {
-      if (!threshold) return "请输入要踢的人数。";
+      if (!threshold || threshold <= 0) return session.text(".no-threshold");
 
       let users = await session.onebot.getGroupMemberList(
         session.guildId,
         true
       );
-      if (threshold < 0 || threshold >= users.length) return "参数不合理。";
-      users = users.sort((a, b) => a.last_sent_time - b.last_sent_time);
-      let title_num = 0;
-      if (options["no-title"]) {
-        users = users.filter((user) => !user.title);
-      }
-      if (threshold < 0 || threshold >= users.length) return "参数不合理。";
-      let target = [];
-      for (let user of users) {
-        if (options["no-title"] && user.title) {
-          title_num++;
-          continue;
-        }
-        target.push(user);
-        if (target.length === threshold) break;
-      }
-      const last_user = target[target.length - 1];
-      await session.send(
-        [
-          `将要清理群友 ${target.length} 人`,
-          options["no-title"] ? `有 ${title_num} 个有头衔的群友逃脱处理` : "",
-          `最后一位群友上次发言的时间：\n${new Date(
-            last_user.last_sent_time * 1000
-          )}`,
-        ].join("\n")
+      // 只留下普通成员和除机器人以外的成员
+      users = users.filter(
+        (user) => user.role === "member" && !ctx.bots[user.user_id]
       );
+      if (threshold > users.length) return session.text(".bad-threshold");
+      users = users.sort((a, b) => a.last_sent_time - b.last_sent_time);
+      const userDict: Record<number, number> = {};
+      users.forEach((user, index) => {
+        userDict[user.user_id] = index;
+      });
+
+      let target = options["no-title"]
+        ? users.filter((user) => !user.title).slice(0, threshold)
+        : users.slice(0, threshold);
+
+      if (target.length === 0) return session.text(".bad-threshold");
+
+      const last_user = target[target.length - 1];
+      const output = [
+        session.text(".alert-normal", [
+          target.length,
+          new Date(last_user.last_sent_time * 1000).toLocaleDateString(),
+        ]),
+      ];
+
+      if (options["no-title"]) {
+        const title_num = userDict[last_user.user_id] - target.length;
+        output.splice(1, 0, session.text(".alert-no-title", [title_num]));
+      }
+      await session.send(output.join("\n"));
       if (options.dry) return;
       for (let index = 0; index < target.length; index++) {
         if (index) await sleep(relex_time * 1000);
@@ -71,6 +71,6 @@ export function apply(ctx: Context, { relex_time }: Config) {
           target[index].user_id
         );
       }
-      return `已踢出群友 ${target.length} 人`;
+      return session.text(".alert-final", [target.length]);
     });
 }
